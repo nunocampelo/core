db35c8e3ba5b6ca5474bc05e04e60ae9f3064c02
campelo <campelo.nuno@gmail.com>
2019-10-22 00:54:56 +0100

    added snapshot implementation

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,9 +1,49 @@
-const snapshot = (url: string, path?: string, selector?: string) => {
+const puppeteer = require('puppeteer')
+import { Browser as puppeteerBrowser } from 'puppeteer'
 
+const snapshot = async (url: string, path: string, selector: string) => {
+
+    const browser: puppeteerBrowser = await puppeteer.launch({ headless: false, executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" });
+    const page = await browser.newPage();
+
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+        deviceScaleFactor: 1,
+    });
+
+    await page.goto(url);
+    const padding: number = 0
+
+    const rect: any = await page.evaluate(selector => {
+        const element = document.querySelector(selector);
+        if (!element)
+            return null
+        const { x, y, width, height } = element.getBoundingClientRect()
+        return { left: x, top: y, width, height, id: element.id }
+    }, selector)
+
+    if (!rect) {
+        throw Error(`Could not find element that matches selector: ${selector}.`)
+    }
+
+    await page.screenshot({
+        path: 'clip.png',
+        clip: {
+            x: rect.left - padding,
+            y: rect.top - padding,
+            width: rect.width + padding * 2,
+            height: rect.height + padding * 2
+        }
+    })
+
+    // await page.screenshot({ path: 'example.png' });
+
+    await browser.close()
 }
 
 export interface Browser {
-    snapshot(url: string, path?: string, selector?: string): void
+    snapshot(url: string, path: string, selector: string): void
 }
 
 export const browser: Browser = {
